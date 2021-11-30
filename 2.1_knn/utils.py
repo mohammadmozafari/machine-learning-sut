245010e87db863c9f1cafdce7ffa583e171dfb5e
mohammad <mozafari.mmd@gmail.com>
2021-11-30 13:44:02 +0330

    Change outputs from dataframe to array

--- 2.1_knn/utils.py
+++ 2.1_knn/utils.py
@@ -5,13 +5,13 @@ def read_data(data_path, target_column):
     df = pd.read_csv(data_path)
     X = df.loc[:, df.columns != target_column]
     Y = df['target']
-    return X, Y
+    return X.values, Y.values
 
 def shuffle(X, Y):
     num_data = X.shape[0]
     mask = list(range(num_data))
     rnd.shuffle(mask)
-    new_X = X.iloc[mask, :]
+    new_X = X[mask, :]
     new_Y = Y[mask]
     return new_X, new_Y
 
@@ -19,3 +19,4 @@ if __name__ == "__main__":
     
     X, Y = read_data('./data/heart.csv', 'target')
     X, Y = shuffle(X, Y)
+    print(X.shape, Y.shape)
