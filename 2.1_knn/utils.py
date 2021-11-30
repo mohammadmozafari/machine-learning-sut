fe875c2dcf810fc8b1e2aedccea79d242d29ae19
mohammad <mozafari.mmd@gmail.com>
2021-11-30 13:51:03 +0330

    Split data into train and test

--- 2.1_knn/utils.py
+++ 2.1_knn/utils.py
@@ -15,8 +15,16 @@ def shuffle(X, Y):
     new_Y = Y[mask]
     return new_X, new_Y
 
+def split_data(X, Y, split_fraction):
+    num_total = X.shape[0]
+    num_train = int(num_total * split_fraction)
+    X_train, Y_train = X[:num_train, :], Y[:num_train]
+    X_val, Y_val = X[num_train:, :], Y[num_train:]
+    return X_train.shape, Y_train.shape, X_val.shape, Y_val.shape
+
 if __name__ == "__main__":
     
     X, Y = read_data('./data/heart.csv', 'target')
     X, Y = shuffle(X, Y)
+    X_train, Y_train, X_val, Y_val = split_data(X, Y, 0.8)
     print(X.shape, Y.shape)
