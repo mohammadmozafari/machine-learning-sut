51217b2cbf3f274127fd29238499030696e8270a
mohammad <mozafari.mmd@gmail.com>
2021-11-30 13:23:32 +0330

    Add read data funcion

--- 2.1_knn/utils.py
+++ 2.1_knn/utils.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-def read_data(idx, target_column):
-    pass
-
+def read_data(data_path, target_column):
+    df = pd.read_csv(data_path)
+    X = df.loc[:, df.columns != target_column]
+    Y = df['target']
+    return X, Y
 
 if __name__ == "__main__":
-    pass
+    
+    read_data('./data/heart.csv', 'target')
